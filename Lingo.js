f3a23ee72936797f8333fa9edc6f1b443f7cbdea
Vincent van Dam <99027519@mydavinci.nl>
2017-02-09 08:35:37 +0100

    no message

---
+++ Lingo.js
@@ -0,0 +1,27 @@
+var pick = Math.floor(Math.random()*words.length-1);
+var guessword = words[pick];
+console.log(guessword);
+
+var CurrentRow = 1;
+var guesswordArr = guessword.split("");
+document.getElementById('letter_'+CurrentRow+'_0').value = guesswordArr[0];
+function check() {
+if (CurrentRow <= 5) {	
+	for (var i = 0; i <= 4; i++) {
+		var input = document.getElementById('letter_'+CurrentRow+'_'+i); // letter_ 2  _ 2
+		if (guesswordArr[i] == input.value) {
+			input.style.backgroundColor = "red";
+		}
+	}
+	CurrentRow++;
+	if (CurrentRow <= 5) {	
+		document.getElementById('letter_'+CurrentRow+'_0').value = guesswordArr[0];
+	}
+	}
+}
+
+function reset() {
+	document.getElementById("table").reset();
+}
+
+
